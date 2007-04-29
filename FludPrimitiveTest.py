a64ee2c95f8bbd56c3cd06d0234f7f0ceef83c2b
Alen Peacock <alenlpeacock@gmail.com>
2007-04-29 04:04:15 +0000

    cleanups to support multiple runs with different file sizes.  add DELETE test back in.

--- FludPrimitiveTest.py
+++ FludPrimitiveTest.py
@@ -13,9 +13,6 @@ Test code for primitive operations.  These ops include all of the descendents
 of ROOT and REQUEST in FludProtocol.
 """
 
-filename = ""
-filekey = ""
-
 def testerror(failure, message, node):
 	"""
 	error handler for test errbacks
@@ -25,33 +22,33 @@ def testerror(failure, message, node):
 	print "At least 1 test FAILED"
 	return failure
 
-def checkDELETE(res, nKu, node, host, port):
+def checkDELETE(res, nKu, fname, fkey, node, host, port):
 	""" checks to ensure the file was deleted """
 	# XXX: check to see that its gone
 	print "all tests PASSED"
 	return res 
 
-def testDELETE(res, nKu, node, host, port):
+def testDELETE(res, nKu, fname, fkey, node, host, port):
 	""" Tests sendDelete, and invokes checkDELETE on success """
 	print "starting testDELETE"
-	deferred = node.client.sendDelete(filekey, host, port, nKu)
-	deferred.addCallback(checkDELETE, nKu, node, host, port)
+	deferred = node.client.sendDelete(fkey, host, port, nKu)
+	deferred.addCallback(checkDELETE, nKu, fname, fkey, node, host, port)
 	deferred.addErrback(testerror, "failed at testDELETE", node)
 	return deferred
 
-def checkVERIFY(res, nKu, node, host, port, hash):
+def checkVERIFY(res, nKu, fname, fkey, node, host, port, hash):
 	""" executes after testVERIFY """
 	if long(hash, 16) != long(res, 16):
 		raise failure.DefaultException("verify didn't match: %s != %s"
 				% (hash, res))
-		#return testDELETE(res, nKu, node, host, port)
+	return testDELETE(res, nKu, fname, fkey, node, host, port)
 
-def testVERIFY(nKu, node, host, port):
+def testVERIFY(nKu, fname, fkey, node, host, port):
 	""" executes after checkRETRIEVE """
 	""" Test sendVerify """
 	print "starting testVERIFY"
 	
-	fd = os.open(filename, os.O_RDONLY)
+	fd = os.open(fname, os.O_RDONLY)
 	fsize = os.fstat(fd)[stat.ST_SIZE]
 	length = 20
 	offset = random.randrange(fsize-length)
@@ -59,73 +56,77 @@ def testVERIFY(nKu, node, host, port):
 	data = os.read(fd, length)
 	os.close(fd)
 	hash = FludCrypto.hashstring(data)
-	deferred = node.client.sendVerify(filekey, offset, length, host, port, nKu)
-	deferred.addCallback(checkVERIFY, nKu, node, host, port, hash)
+	deferred = node.client.sendVerify(fkey, offset, length, host, port, nKu)
+	deferred.addCallback(checkVERIFY, nKu, fname, fkey, node, host, port, hash)
 	deferred.addErrback(testerror, "failed at testVERIFY", node)
 	return deferred
 
-def checkRETRIEVE(res, nKu, node, host, port):
+def checkRETRIEVE(res, nKu, fname, fkey, node, host, port):
 	""" Compares the file that was stored with the one that was retrieved """
-	f1 = open(filename)
-	f2 = open(os.path.join(node.config.clientdir,filekey))
+	f1 = open(fname)
+	f2 = open(os.path.join(node.config.clientdir, fkey))
 	if (f1.read() != f2.read()):
 		f1.close()
 		f2.close()
 		raise failure.DefaultException("upload/download files don't match")
 	f1.close()
 	f2.close()
-	return testVERIFY(nKu, node, host, port)
+	return testVERIFY(nKu, fname, fkey, node, host, port)
 
-def testRETRIEVE(res, nKu, node, host, port):
+def testRETRIEVE(res, nKu, fname, fkey, node, host, port):
 	""" Tests sendRetrieve, and invokes checkRETRIEVE on success """
 	print "starting testRETRIEVE"
-	deferred = node.client.sendRetrieve(filekey, host, port, nKu)
-	deferred.addCallback(checkRETRIEVE, nKu, node, host, port)
+	deferred = node.client.sendRetrieve(fkey, host, port, nKu)
+	deferred.addCallback(checkRETRIEVE, nKu, fname, fkey, node, host, port)
 	deferred.addErrback(testerror, "failed at testRETRIEVE", node)
 	return deferred
 
-def testSTORE(nKu, node, host, port):
+def testSTORE(nKu, fname, fkey, node, host, port):
 	""" Tests sendStore, and invokes testRETRIEVE on success """
 	print "starting testSTORE"
-	deferred = node.client.sendStore(filename, host, port, nKu)
-	deferred.addCallback(testRETRIEVE, nKu, node, host, port)
+	deferred = node.client.sendStore(fname, host, port, nKu)
+	deferred.addCallback(testRETRIEVE, nKu, fname, fkey, node, host, port)
 	deferred.addErrback(testerror, "failed at testSTORE", node)
 	return deferred
 
-def testID(node, host, port):
+def testID(fname, fkey, node, host, port):
 	""" Tests sendGetID(), and invokes testSTORE on success """
 	print "starting testID"
 	deferred = node.client.sendGetID(host, port)
-	deferred.addCallback(testSTORE, node, host, port)
+	deferred.addCallback(testSTORE, fname, fkey, node, host, port)
 	deferred.addErrback(testerror, "failed at testID", node)
 	return deferred
 
 	
-def cleanup(_, node):
-	os.remove(filename)
+def cleanup(_, node, filenamelist):
+	for f in filenamelist:
+		try:
+			os.remove(f)
+		except:
+			print "couldn't remove %s" % f
 	reactor.callLater(1, node.stop)
 
-def generateTestData():
-	global filename
-	global filekey
+def generateTestData(minSize):
 	fname = tempfile.mktemp()
 	f = open(fname, 'w')
-	for i in range(0, random.randrange(10)+1):
-		f.write(FludCrypto.generateRandom(1024))
+	data = FludCrypto.generateRandom(minSize/50)
+	for i in range(0, 51+random.randrange(50)):
+		f.write(data)
 	f.close()
 	filekey = FludCrypto.hashfile(fname)
 	filekey = fencode(int(filekey, 16))
 	filename = os.path.join("/tmp",filekey)
 	os.rename(fname,filename)
+	return (filename, filekey)
 
 def runTests(host, port=None, listenport=None):
-	generateTestData()
+	(largeFilename, largeFilekey) = generateTestData(512000)
 	node = FludNode(port=listenport)
 	if port == None:
 		port = node.config.port
 	node.run()
-	d = testID(node, host, port)
-	d.addBoth(cleanup, node)
+	d = testID(largeFilename, largeFilekey, node, host, port)
+	d.addBoth(cleanup, node, [largeFilename,])
 	node.join()
 
 """

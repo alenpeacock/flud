6f038c27aff8324915038d6c862877a09c25d3a8
Alen Peacock <alenlpeacock@gmail.com>
2007-05-17 06:01:57 +0000

    clean dl directory recursively

--- cleanNodes.sh
+++ cleanNodes.sh
@@ -3,4 +3,4 @@
 for i in ~/.flud*/dht/*; do rm $i; done
 for i in ~/.flud*/store/*; do rm $i; done
 for i in ~/.flud*/meta/*; do rm $i; done
-for i in ~/.flud*/dl/*; do rm $i; done
+for i in ~/.flud*/dl/*; do rm -r $i; done

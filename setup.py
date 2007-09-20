24a2c24629a13d58e4e621dbb2ccffde5d24fe83
Alen Peacock <alenlpeacock@gmail.com>
2007-09-20 05:40:14 +0000

    make root setup.py capable of doin' the ldpc wrappers

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+
 setup(name="Flud",
 		version="0.0.2", 
 		description="Flud Backup", 
 		author="Alen Peacock",
 		author_email="apeacock@flud.org",
 		url='http://flud.org',
-		packages=['', 'Protocol'],)
+		packages=['', 'Protocol'],
+		ext_package='filecoder',
+		ext_modules=[Extension('filecoder',
+			sources = ['coding/filecodermodule.cpp'],
+			extra_objects = ['coding/CodedBlocks.o', 'coding/Coder.o', 'coding/Decoder.o'],
+			include_dirs = ['coding/ldpc/src'],
+			libraries = ['ldpc', 'stdc++'],
+			library_dirs = ['coding/ldpc/bin/linux'],
+			language = ['c++'])]
+		)
 
 

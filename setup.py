135bc6b061f05f3877ed2382f3a582a1de60f017
Alen Peacock <alenlpeacock@gmail.com>
2007-10-05 04:55:54 +0000

    remove old ldpc coder/decoder

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
 from distutils.core import setup, Extension
 import os, sys
 
-class AutoToolsExtension(Extension):
-	'''
-	provides simple hooks for running ./configure, make, and any pre-configure
-	scripts (such as ./bootstrap) necessary to build an Extension package.
-	'''
-	def __init__(self, name, sources=[], extra_objects=[], include_dirs=[], 
-			libraries=[], library_dirs=[], language=[], pre_configure=[],
-			run_configure=[], run_make=[]):
-		for dir, cmd in pre_configure:
-			r = os.system('cd %s; %s' % (dir, cmd))
-			if r != 0: sys.exit()
-		for dir in run_configure:
-			r = os.system('cd %s; ./configure' % dir)
-			if r != 0: sys.exit()
-		for dir in run_make:
-			r = os.system('cd %s; make' % dir)
-			if r != 0: sys.exit()
-		Extension.__init__(self, name, sources=sources,
-				extra_objects=extra_objects, include_dirs=include_dirs,
-				libraries=libraries, library_dirs=library_dirs,
-				language=language)
-
 setup(name="flud",
 		version="0.0.2", 
 		description="flud decentralized backup", 
@@ -56,26 +34,11 @@ setup(name="flud",
 			'flud/bin/stop-fludnodes', 
 			'flud/bin/gauges-fludnodes',
 			'flud/bin/clean-fludnodes'],
-		ext_modules=[
-			#AutoToolsExtension('filecoder',
-			#	pre_configure = [('flud/coding', './bootstrap')],
-			#	run_configure = ['flud/coding'],
-			#	run_make = ['flud/coding/ldpc', 'flud/coding'],
-			#	sources = ['flud/coding/filecodermodule.cpp'],
-			#	extra_objects = ['flud/coding/CodedBlocks.o', 
-			#		'flud/coding/Coder.o',
-			#		'flud/coding/Decoder.o'],
-			#	include_dirs = ['flud/coding/ldpc/src', 'flud/coding'],
-			#	libraries = ['ldpc', 'stdc++'],
-			#	library_dirs = ['flud/coding/ldpc/bin/linux'],
-			#	language = ['c++']),
-			Extension('_fec', 
-				['flud/zfec/fec.c', 'flud/zfec/_fecmodule.c',], 
-				extra_link_args=[], 
-				extra_compile_args=['-std=c99'], 
-				undef_macros=[]
-				)	
-			]
-		)
+		ext_modules=[Extension('_fec', 
+			['flud/zfec/fec.c', 'flud/zfec/_fecmodule.c',], 
+			extra_link_args=[], 
+			extra_compile_args=['-std=c99'], 
+			undef_macros=[])]
+)
 
 
